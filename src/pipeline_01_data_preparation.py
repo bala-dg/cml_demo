1a4b7a187ac60641207282592ab0387abc5aa451
bala-bc <balaganesh.gurunathan@brickendon.com>
2022-06-29 17:43:45 +0530

    read config

--- src/pipeline_01_data_preparation.py
+++ src/pipeline_01_data_preparation.py
@@ -4,16 +4,22 @@ import yaml
 import logging
 
 
+def read_params(config_path):
+    with open(config_path) as yaml_file:
+        config = yaml.safe_load(yaml_file)
+    return config
 
 
+def main(config_path, datasource):
+    config = read_params(config_path)
+    print(config)
 
 
-
-
-if __name__=="__main__":
+if __name__ == "__main__":
     args = argparse.ArgumentParser()
-    args.add_argument("--config", default='default')
+    default_config_path = os.path.join('config', "params.yml")
+    args.add_argument("--config", default=default_config_path)
     args.add_argument("--datasource", default=None)
 
     parsed_arg = args.parse_args()
-    print(parsed_arg)
+    main(config_path=parsed_arg.config, datasource=parsed_arg.datasource)
